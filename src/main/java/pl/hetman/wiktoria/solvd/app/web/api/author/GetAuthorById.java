f74e10af41c25b6a686c55777232e6cff702e7b3
wiktoriahet <wiktoriaspyc@gmail.com>
2024-02-08 18:23:28 +0100

    refactored packages

--- src/main/java/pl/hetman/wiktoria/solvd/app/web/api/author/GetAuthorById.java
+++ src/main/java/pl/hetman/wiktoria/solvd/app/web/api/author/GetAuthorById.java
@@ -1,18 +1,20 @@
-package pl.hetman.wiktoria.solvd.app.api.author;
+package pl.hetman.wiktoria.solvd.app.web.api.author;
 
 import com.zebrunner.carina.api.AbstractApiMethodV2;
 import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
 import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
 import com.zebrunner.carina.api.apitools.builder.NotStringValuesProcessor;
 import com.zebrunner.carina.api.http.HttpMethodType;
 import com.zebrunner.carina.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.utils.config.Configuration;
 
-@Endpoint(url = "${base_url}api/v1/Authors/${authorId}", methodType = HttpMethodType.DELETE)
+@Endpoint(url = "${base_url}api/v1/Authors/${authorId}", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "api/authors/_get/rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
-public class DeleteAuthorById extends AbstractApiMethodV2 {
+public class GetAuthorById extends AbstractApiMethodV2 {
 
-    public DeleteAuthorById(Integer authorId) {
+    public GetAuthorById(Integer authorId) {
         replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
         replaceUrlPlaceholder("authorId", authorId.toString());
         ignorePropertiesProcessor(NotStringValuesProcessor.class);
--- src/main/java/pl/hetman/wiktoria/solvd/app/web/api/author/PostAuthor.java
+++ src/main/java/pl/hetman/wiktoria/solvd/app/web/api/author/PostAuthor.java
@@ -1,4 +1,4 @@
-package pl.hetman.wiktoria.solvd.app.api.author;
+package pl.hetman.wiktoria.solvd.app.web.api.author;
 
 import com.zebrunner.carina.api.AbstractApiMethodV2;
 import com.zebrunner.carina.api.annotation.Endpoint;
